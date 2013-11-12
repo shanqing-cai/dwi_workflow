e1c1dafb85a0c0e70ad46523fd0a2dc31d5b165d
shanqing-cai <shanqing.cai@gmail.com>
2013-11-12 00:29:50 -0500

    Working on the group-level analysis script

--- dwi_group.py
+++ dwi_group.py
@@ -6,6 +6,7 @@ import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 from scai_utils import *
 
@@ -13,7 +14,7 @@ from dwi_analysis_settings import DWI_ANALYSIS_DIR
 from dwi_project_info import projInfo
 
 COLORS = [[0, 0, 1], [0, 1, 0], [1, 0, 0], [0, 0.5, 1],
-          [0.5, 1, 0], [0.5, 0, 1], [1, 1, 0], [0, 1, 1],
+          [0.5, 1, 0], [0.5, 0, 1], [0.5, 0.5, 0], [0, 1, 1],
           [1, 0.25, 0.75], [0.25, 1, 0.75], [0.75, 0.25, 1], [1, 0.75, 0.25],
           [0.5, 0.5, 0.25], [0.5, 0.25, 0.75], [0.75, 0.25, 0.5], [0.75, 0.5, 0.5]]
 
@@ -23,7 +24,7 @@ COLORS = [[0, 0, 1], [0, 1, 0], [1, 0, 0], [0, 0.5, 1],
 ANALYSIS_TYPES = ["FA", "MD"]
 
 # All grouping schemes
-GROUPING_SCHEMES = ["byStudy"]
+GROUPING_SCHEMES = ["byStudy", "byGroup"]
 
 def makeOptList(lst):
     str = "{"
@@ -76,6 +77,13 @@ if __name__ == "__main__":
                     dest="roi")
     ap.add_argument("--wmDepth", type=int, 
                     help="White-matter depth (e.g., -1 for gm, 1, 2, 3)")
+    ap.add_argument("--groups", type=str, dest="groups",
+                    help="Groups to analyze " + \
+                         "(compulsary for the grouping scheme byGroup; " + \
+                         "optional for other groupig schcmes )")
+    ap.add_argument("--exclude-subjects", type=str, dest="exclS", 
+                    help="Exclude subjects, " + \
+                         "e.g., --exclude-subjects CAT_12,SEQPDS_SEQ02P10")
     ap.add_argument("-v", help="Verbose mode",
                     dest="bVerbose", action="store_true")
 
@@ -91,6 +99,15 @@ if __name__ == "__main__":
     if GROUPING_SCHEMES.count(args.grpScheme) != 1:
         error_log("Unrecognized grouping scheme: %s" % args.grpScheme)
 
+    #=== Build list of subjects to be excluded ===#
+    sExcludeList = []
+    if args.exclS != None:
+        sExcludeList = args.exclS.split(",")
+
+    anaGroups = [] # Groups to be included in the analysis 
+    if args.groups != None:
+        anaGroups = args.groups.split(",")
+
     #=== Check conditionally required arguments ===#
     if args.anaType == "FA" or args.anaType == "MD":
         if args.roi == None:
@@ -99,22 +116,42 @@ if __name__ == "__main__":
         if args.wmDepth == None:
             error_log("The option --wmDepth is required for the analysis type %s" % args.anaType)
 
+    if args.grpScheme == "byGroup" \
+       and (args.groups == None or len(args.groups) == 0):
+        error_log("--groups not specified under grouping scheme: %s" \
+                  % args.grpScheme)
+
     #=== Load data ===#    
     dat = [] # Data
-    grp = [] # Group info
+    grp = [] # Group info: it can hold things like study ID and group ID
     
     for (i0, t_proj) in enumerate(projInfo["name"]):
         ns = len(projInfo["subjIDs"][i0]) # Number of subjects
-        nsa = 0 # Number of subjects with data available 
+        nsa = 0 # Number of subjects with data available
         
         if args.bVerbose:
             info_log("Loading data from project %s" % t_proj)
 
-
         for (i1, t_sid) in enumerate(projInfo["subjIDs"][i0]):
             t_psid = "%s_%s" % (t_proj, t_sid)
+
+            #== Exclude subject per input argument ==#
+            if sExcludeList.count(t_psid) > 0:
+                if args.bVerbose:
+                    info_log("Excluding subject %s (request in options)" \
+                             % t_psid)
+                continue
+
+            #== Check the group identity of the subject ==#
+            t_grp_id = projInfo["groupIDs"][i0][i1]
+            if len(anaGroups) > 0 and anaGroups.count(t_grp_id) == 0:
+                if args.bVerbose:
+                    info_log("Excluding subject %s (outside groups to be analyzed)" % t_psid)
+                    continue
+
             sDir = os.path.join(DWI_ANALYSIS_DIR, t_psid)
-            
+
+            #=== Load data ===#
             if args.anaType == "FA" or args.anaType == "MD":
                 # Tensor measure file
                 tmfile = os.path.join(sDir, "tensor", "tensor_measures.mat")
@@ -128,16 +165,24 @@ if __name__ == "__main__":
                     continue
                 
                 dat.append(t_dat)
-                grp.append(i0)
-                
-                nsa += 1
-                
+               
             else:
                 error_log("Analaysis type %s has not been implemented yet" \
                           % args.anaType)
 
+            #=== Add group identity info ===#
+            if args.grpScheme == "byStudy":
+                grp.append(i0)
+            elif args.grpScheme == "byGroup":
+                grp.append(anaGroups.index(t_grp_id))
+            else:
+                error_log("Unrecognized grouping scheme: %s" \
+                          % args.grpScheme)
+            
+            nsa += 1
+
         if args.bVerbose:
-            info_log("\tData are available from %s of %s subjects" % (nsa, ns))
+            info_log("\tData loaded from %s of %s subjects" % (nsa, ns))
 
     #=== Clean-up data ===#
     dat = np.array(dat)
@@ -145,26 +190,62 @@ if __name__ == "__main__":
 
 
     #=== Statistical analysis and visualizaton ===#
+    import scipy.stats as stats
     if args.grpScheme == "byStudy":
-        import scipy.stats as stats
-
-#        (F, p) = stats.f_oneway(dat)
-
-        
-        plt.figure()
+#        plt.figure()
 
         ugrps = np.unique(grp)
-        
+
+        plt.figure(facecolor=[1.0, 1.0, 1.0])
+        datArray = []
         for (i0, t_grp) in enumerate(ugrps):
             idx = [i for (i, x) in enumerate(grp) if x == t_grp]
+            
+            if len(idx) > 0:
+                datArray.append(dat[idx])
+                plt.plot(idx, dat[idx], "o-", color=COLORS[i0])
+                plt.text(idx[0] + 0.5, np.min(dat[idx]) * 0.95,
+                         projInfo["name"][t_grp],
+                         color=COLORS[i0])
+
+        plt.xlabel("Subject #")
+        plt.ylabel(args.anaType + ": " + args.roi)
+        
+        (ANOVA_F, ANOVA_p) = stats.f_oneway(*datArray)
+
+        xs = plt.xlim()
+        ys = plt.ylim()
+
+        plt.text(xs[0] + 0.05 * (xs[1] - xs[0]),
+                 ys[1] - 0.075 * (ys[1] - ys[0]),
+                 "One-way ANOVA: F = %f; p = %e" % (ANOVA_F, ANOVA_p))
+
+        plt.show()
+
+    elif args.grpScheme == "byGroup":
+        plt.figure(facecolor=[1.0, 1.0, 1.0])
+
+        ugrps = np.unique(grp)
+
+        for (i0, t_grp) in enumerate(anaGroups):
+            idx = [i for (i, x) in enumerate(grp) if x == i0]
 
             if len(idx) > 0:
-                plt.plot(idx, dat[idx], "o-", color=COLORS[i0])
-                plt.text(idx[0], dat[idx[0]], projInfo["name"][t_grp],
-                         color=COLORS[i0])
-        
+                plt.plot(i0 + 1, np.mean(dat[idx]), "o", color=COLORS[i0])
+                plt.plot([i0 + 1, i0 + 1], 
+                         np.mean(dat[idx]) + \
+                            np.array([-1, 1]) * np.std(dat[idx]) \
+                               / np.sqrt(len(idx)),
+                         "-", color=COLORS[i0])
+        plt.xlim([0, len(anaGroups) + 1])
+        plt.xticks(np.linspace(1, len(anaGroups), len(anaGroups)))
+        plt.xlabel("Groups ")
+        # plt.set_xticklabels(anaGroups)
+#                plt.text(idx[0], dat[idx[0]] * 1.2, anaGroups[i0],
+#                         color=COLORS[i0])
+
         plt.show()
-        plt.ylim([0, 0.5])
+        
     else:
         error_log("Grouping scheme %s has not been implemented yet" \
                   % args.grpScheme)
